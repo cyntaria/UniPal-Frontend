9cdf0a0ae4a3e185068966a7bef14acdc6ba1a5b
arafaysaleem <a.rafaysaleem@gmail.com>
2022-01-25 01:36:48 +0500

    Initial Commit

---
+++ android/app/src/main/kotlin/com/cyntaria/unipal_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.cyntaria.unipal_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ test/flutter_test_config.dart
@@ -0,0 +1,33 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:golden_toolkit/golden_toolkit.dart';
+
+Future<void> testExecutable(FutureOr<void> Function() testMain) async {
+  return GoldenToolkit.runWithConfiguration(
+    () async {
+      await loadAppFonts();
+      await testMain();
+    },
+    config: GoldenToolkitConfiguration(
+      defaultDevices: const [GoldensGlobalConfig.defaultDevice],
+      fileNameFactory: (name) {
+        if (Platform.isWindows) {
+          return 'goldens_local/$name.png';
+        } else {
+          return 'goldens/$name.png';
+        }
+      },
+    ),
+  );
+}
+
+abstract class GoldensGlobalConfig {
+  static final globalAppWrapper = materialAppWrapper(
+      // theme: CustomTheme.mainTheme,
+      );
+
+  static const defaultDevice = Device.iphone11;
+
+  static final defaultSurfaceSize = defaultDevice.size;
+}
