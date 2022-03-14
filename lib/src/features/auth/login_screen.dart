ac8883c5ff6c0b06c319bfec493ed3de3d58b241
arafaysaleem <a.rafaysaleem@gmail.com>
2022-03-14 05:08:55 +0500

    add(Login-Screen): add widget tree guidelines

--- lib/src/features/auth/login_screen.dart
+++ lib/src/features/auth/login_screen.dart
@@ -1,12 +1,45 @@
 import 'package:flutter/material.dart';
 
+// Widgets
+import '../../helpers/constants/app_styles.dart';
+import '../shared/widgets/scrollable_column.dart';
+
 class LoginScreen extends StatelessWidget {
   const LoginScreen({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(),
+    return const Scaffold(
+      body: ScrollableColumn(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // Logo
+
+          Insets.gapH30,
+
+          // ERP-Email Input
+
+          Insets.gapH10,
+
+          // Password Input
+
+          Insets.gapH5,
+
+          // Forgot Password Link
+
+          Insets.gapH10,
+
+          // Login Button
+
+          Insets.gapH15,
+
+          // Divider with "or"
+
+          Insets.gapH15,
+
+          // Signup Button
+        ],
+      ),
     );
   }
 }
