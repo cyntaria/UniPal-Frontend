81e0e1c72ff5862a57cba8097ed7682a77a595da
arafaysaleem <a.rafaysaleem@gmail.com>
2022-03-22 06:06:27 +0500

    add(Helpers): add assets helper

---
+++ lib/src/helpers/constants/app_assets.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/foundation.dart';
+
+/// A utility class that holds constants for assets strings.
+/// This class has no constructor and all variables are `static`.
+@immutable
+class AppAssets {
+  const AppAssets._();
+
+  /// The path for face id image asset
+  static const String faceId = 'assets/face_id.png';
+
+  /// The path for Pakistani flag image asset
+  static const String pkFlag = 'assets/pk_flag.png';
+}
