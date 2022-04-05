b8abe0f9f7da9b1b8f3c1fca85e36349725d1894
arafaysaleem <a.rafaysaleem@gmail.com>
2022-04-06 04:28:08 +0500

    add(Shared): add custom adaptive date picker

---
+++ lib/src/features/shared/widgets/cupertino_date_picker_dialog.dart
@@ -0,0 +1,175 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+// Helpers
+import '../../../helpers/constants/app_colors.dart';
+import '../../../helpers/constants/app_styles.dart';
+import '../../../helpers/constants/app_typography.dart';
+
+class CupertinoDatePickerDialog extends StatefulWidget {
+  /// The mode of the date picker as one of [CupertinoDatePickerMode].
+  /// Defaults to [CupertinoDatePickerMode.dateAndTime]. Cannot be null and
+  /// value cannot change after initial build.
+  final CupertinoDatePickerMode mode;
+
+  /// The initial date and/or time of the picker. Defaults to the present date
+  /// and time and must not be null. The present must conform to the intervals
+  /// set in [minimumDate], [maximumDate], [minimumYear], and [maximumYear].
+  ///
+  /// Changing this value after the initial build will not affect the currently
+  /// selected date time.
+  final DateTime initialDateTime;
+
+  /// The minimum selectable date that the picker can settle on.
+  ///
+  /// The user can still scroll the picker to [DateTime]s earlier
+  /// than [minimumDate], but the [onDateTimeChanged] will not be called on
+  /// these [DateTime]s. Once let go, the picker will scroll back to [minimumDate].
+  ///
+  /// In [CupertinoDatePickerMode.time] mode, a time becomes unselectable if the
+  /// [DateTime] produced by combining that particular time and the date part of
+  /// [initialDateTime] is earlier than [minimumDate]. So typically [minimumDate]
+  /// needs to be set to a [DateTime] that is on the same date as [initialDateTime].
+  final DateTime minimumDate;
+
+  /// The maximum selectable date that the picker can settle on.
+  ///
+  /// The user can still scroll the picker to [DateTime]s later
+  /// than [maximumDate], but the [onDateTimeChanged] will not be called on
+  /// these [DateTime]s. Once let go, the picker will scroll back to [maximumDate].
+  ///
+  /// In [CupertinoDatePickerMode.time] mode, a time becomes unselectable if the
+  /// [DateTime] produced by combining that particular time and the date part of
+  /// [initialDateTime] is later than [maximumDate]. So typically [maximumDate]
+  /// needs to be set to a [DateTime] that is on the same date as [initialDateTime].
+  final DateTime maximumDate;
+
+  /// Background color of date picker dialog.
+  ///
+  /// Defaults to null, which disables background painting entirely.
+  final Color? backgroundColor;
+
+  /// The text that is displayed at the top of the header.
+  ///
+  /// This is used to indicate to the user what they are selecting a date for.
+  final String? helpText;
+
+  const CupertinoDatePickerDialog({
+    Key? key,
+    this.helpText,
+    this.backgroundColor,
+    this.mode = CupertinoDatePickerMode.date,
+    required this.initialDateTime,
+    required this.minimumDate,
+    required this.maximumDate,
+  }) : super(key: key);
+
+  @override
+  _CupertinoDatePickerDialogState createState() =>
+      _CupertinoDatePickerDialogState();
+}
+
+class _CupertinoDatePickerDialogState extends State<CupertinoDatePickerDialog> {
+  late DateTime _selectedDate;
+
+  @override
+  void initState() {
+    _selectedDate = widget.initialDateTime;
+    super.initState();
+  }
+
+  void _onCancel() {
+    return Navigator.of(context).pop(widget.initialDateTime);
+  }
+
+  void _onDone() {
+    return Navigator.of(context).pop(_selectedDate);
+  }
+
+  void _selectPickedDate(DateTime picked) {
+    setState(() {
+      _selectedDate = picked;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 250,
+      margin: EdgeInsets.only(
+        bottom: MediaQuery.of(context).viewInsets.bottom,
+      ),
+      color: widget.backgroundColor ?? AppColors.backgroundColor,
+      child: SafeArea(
+        top: false,
+        child: Column(
+          children: [
+            // Options Row
+            Material(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  // Cancel Link
+                  GestureDetector(
+                    onTap: _onCancel,
+                    child: Padding(
+                      padding: const EdgeInsets.all(15),
+                      child: Text(
+                        'Cancel',
+                        style: AppTypography.primary.body14.copyWith(
+                          color: AppColors.textGreyColor,
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  // Help Text
+                  Text(
+                    widget.helpText ?? 'Select A Date',
+                    style: AppTypography.primary.body16.copyWith(
+                      fontSize: 17,
+                      color: AppColors.textBlackColor,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+
+                  // Done Link
+                  GestureDetector(
+                    onTap: _onDone,
+                    child: Padding(
+                      padding: const EdgeInsets.all(15),
+                      child: Text(
+                        'Done',
+                        style: AppTypography.primary.body14.copyWith(
+                          color: AppColors.primaryColor,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            const Divider(
+              height: 0,
+              thickness: 1.2,
+              color: Color(0xFFE0E0E0),
+            ),
+
+            Insets.gapH5,
+
+            Expanded(
+              child: CupertinoDatePicker(
+                mode: widget.mode,
+                onDateTimeChanged: _selectPickedDate,
+                initialDateTime: _selectedDate,
+                minimumDate: widget.minimumDate,
+                maximumDate: widget.maximumDate,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/src/features/shared/widgets/custom_date_picker.dart
@@ -0,0 +1,156 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+// Helpers
+import '../../../helpers/constants/app_colors.dart';
+import '../../../helpers/constants/app_styles.dart';
+import '../../../helpers/constants/app_typography.dart';
+
+// Widgets
+import './cupertino_date_picker_dialog.dart';
+import './custom_text_button.dart';
+
+class CustomDatePicker extends StatelessWidget {
+  /// The initially selected [DateTime] that the picker should display.
+  final DateTime? initialDate;
+
+  /// The earliest allowable [DateTime] that the user can select.
+  final DateTime firstDate;
+
+  /// The latest allowable [DateTime] that the user can select.
+  final DateTime? lastDate;
+
+  /// The [DateTime] representing today. It will be highlighted in the day grid.
+  final DateTime? currentDate;
+
+  /// The initial mode of date entry method for the date picker dialog.
+  ///
+  /// See [DatePickerEntryMode] for more details on the different data entry
+  /// modes available.
+  final DatePickerEntryMode initialEntryMode;
+
+  /// Function to provide full control over which [DateTime] can be selected.
+  final SelectableDayPredicate? selectableDayPredicate;
+
+  /// The text that is displayed at the top of the header.
+  ///
+  /// This is used to indicate to the user what they are selecting a date for.
+  final String? helpText;
+
+  /// The initial display of the calendar picker.
+  final DatePickerMode initialMaterialDatePickerMode;
+
+  /// The mode of the date picker as one of [CupertinoDatePickerMode].
+  /// Defaults to [CupertinoDatePickerMode.dateAndTime]. Cannot be null and
+  /// value cannot change after initial build.
+  final CupertinoDatePickerMode initialCupertinoDatePickerMode;
+
+  /// The notifier used to store and passback selected values to the parent.
+  final ValueNotifier<DateTime?> dateNotifier;
+
+  /// The format used to decide the pattern of the selected datetime's
+  /// output in the form field.
+  final DateFormat dateFormat;
+
+  final CustomDatePickerStyle pickerStyle;
+
+  const CustomDatePicker({
+    Key? key,
+    this.lastDate,
+    this.helpText,
+    this.currentDate,
+    this.initialDate,
+    this.selectableDayPredicate,
+    this.initialEntryMode = DatePickerEntryMode.calendar,
+    this.initialMaterialDatePickerMode = DatePickerMode.day,
+    this.initialCupertinoDatePickerMode = CupertinoDatePickerMode.date,
+    this.pickerStyle = const CustomDatePickerStyle(),
+    required this.firstDate,
+    required this.dateNotifier,
+    required this.dateFormat,
+  }) : super(key: key);
+
+  Future<void> _pickDate(BuildContext context) async {
+    final nowDate = DateTime.now();
+    if (!Platform.isIOS) {
+      dateNotifier.value = await showDatePicker(
+            helpText: helpText,
+            context: context,
+            initialEntryMode: initialEntryMode,
+            initialDatePickerMode: initialMaterialDatePickerMode,
+            initialDate: dateNotifier.value ?? initialDate ?? nowDate,
+            firstDate: firstDate,
+            lastDate: lastDate ?? nowDate,
+          ) ??
+          nowDate;
+    } else {
+      dateNotifier.value = await showCupertinoModalPopup<DateTime>(
+        context: context,
+        builder: (BuildContext ctx) {
+          return CupertinoDatePickerDialog(
+            mode: initialCupertinoDatePickerMode,
+            initialDateTime: dateNotifier.value ?? initialDate ?? nowDate,
+            minimumDate: firstDate,
+            maximumDate: lastDate ?? nowDate,
+          );
+        },
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomTextButton(
+      width: double.infinity,
+      height: 47,
+      onPressed: () => _pickDate(context),
+      color: pickerStyle.displayFieldColor,
+      padding: const EdgeInsets.only(left: 20, right: 15),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          ValueListenableBuilder<DateTime?>(
+            valueListenable: dateNotifier,
+            builder: (_, date, __) {
+              var dateString = pickerStyle.initialDateString;
+              if (date != null) {
+                dateString = dateFormat.format(date);
+              }
+              return Text(
+                dateString,
+                style: pickerStyle.displayTextStyle,
+              );
+            },
+          ),
+
+          // Icon
+          pickerStyle.icon,
+        ],
+      ),
+    );
+  }
+}
+
+class CustomDatePickerStyle {
+  final Widget icon;
+  final TextStyle displayTextStyle;
+  final Color displayFieldColor;
+  final String initialDateString;
+
+  const CustomDatePickerStyle({
+    this.icon = const Icon(
+      Icons.calendar_month_rounded,
+      color: AppColors.primaryColor,
+      size: IconSizes.med22,
+    ),
+    this.displayTextStyle = const TextStyle(
+      fontSize: FontSizes.f16,
+      color: AppColors.textGreyColor,
+    ),
+    this.displayFieldColor = AppColors.surfaceColor,
+    this.initialDateString = 'Select a date',
+  });
+}
