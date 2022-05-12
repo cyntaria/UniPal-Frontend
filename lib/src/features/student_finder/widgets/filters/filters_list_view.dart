e70c5cb10f010bf69f28d69fe0935a3499690be8
arafaysaleem <a.rafaysaleem@gmail.com>
2022-05-13 02:49:56 +0500

    refact(StudentFinder): load filter values from providers

--- lib/src/features/student_finder/widgets/filters/filters_list_view.dart
+++ lib/src/features/student_finder/widgets/filters/filters_list_view.dart
@@ -6,14 +6,18 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import '../../../auth/enums/gender_enum.dart';
 
 // Models
+import '../../../profile/models/campus_model.codegen.dart';
 import '../../../profile/models/hobby_model.codegen.dart';
 import '../../../profile/models/interest_model.codegen.dart';
+import '../../../profile/models/program_model.codegen.dart';
+import '../../../profile/models/student_status_model.codegen.dart';
 
 // Providers
-import '../../../profile/providers/campus_provider.dart';
+import '../../../profile/providers/campuses_provider.dart';
 import '../../../profile/providers/hobbies_provider.dart';
 import '../../../profile/providers/interests_provider.dart';
-import '../../../profile/providers/program_provider.dart';
+import '../../../profile/providers/programs_provider.dart';
+import '../../../profile/providers/student_statuses_provider.dart';
 
 // Helpers
 import '../../../../helpers/constants/app_styles.dart';
@@ -57,29 +61,39 @@ class FiltersListView extends HookConsumerWidget {
         Insets.gapH20,
 
         // Program Dropdown Filter
-        LabeledWidget(
-          label: 'Program',
-          useDarkerLabel: true,
-          child: CustomDropdownField<int>.animated(
-            controller: programController,
-            hintText: 'Select a program',
-            items: programs.map((key, value) => MapEntry(value, key)),
-            onSelected: (id) {},
-          ),
+        Consumer(
+          builder: (context, ref, child) {
+            final programs = ref.watch(programsProvider).getAllPrograms();
+            return LabeledWidget(
+              label: 'Program',
+              useDarkerLabel: true,
+              child: CustomDropdownField<ProgramModel>.animated(
+                controller: programController,
+                hintText: 'Select a program',
+                items: {for (var e in programs) e.program: e},
+                onSelected: (program) {},
+              ),
+            );
+          },
         ),
 
         Insets.gapH20,
 
         // Campuses Dropdown Filter
-        LabeledWidget(
-          label: 'Campus',
-          useDarkerLabel: true,
-          child: CustomDropdownField<int>.animated(
-            controller: campusController,
-            hintText: 'Select a campus',
-            items: campuses.map((key, value) => MapEntry(value, key)),
-            onSelected: (id) {},
-          ),
+        Consumer(
+          builder: (context, ref, child) {
+            final campuses = ref.watch(campusesProvider).getAllCampuses();
+            return LabeledWidget(
+              label: 'Campus',
+              useDarkerLabel: true,
+              child: CustomDropdownField<CampusModel>.animated(
+                controller: campusController,
+                hintText: 'Select a campus',
+                items: {for (var e in campuses) e.campus: e},
+                onSelected: (campus) {},
+              ),
+            );
+          },
         ),
 
         Insets.gapH20,
@@ -92,79 +106,70 @@ class FiltersListView extends HookConsumerWidget {
             controller: batchController,
             enableSearch: true,
             hintText: 'Select a batch',
-            items: const {
-              '2025': 2025,
-              '2024': 2024,
-              '2023': 2023,
-              '2022': 2022,
-              '2021': 2021,
-              '2020': 2020,
-            },
-            onSelected: (id) {},
+            items: {for (var i = 2050; i >= 1950; i--) '$i': i},
+            onSelected: (year) {},
           ),
         ),
 
         Insets.gapH20,
 
         // Hobbies Dropdown Filter
-        LabeledWidget(
-          label: 'Hobby',
-          useDarkerLabel: true,
-          child: CustomDropdownField<HobbyModel>.animated(
-            controller: hobbyController,
-            enableSearch: true,
-            hintText: 'Select a hobby',
-            items: ref
-                .watch(
-                  hobbiesProvider.select((value) => value.getHobbiesMap()),
-                )
-                .map((k, v) => MapEntry(v.hobby, v)),
-            onSelected: (id) {},
-          ),
+        Consumer(
+          builder: (context, ref, child) {
+            final hobbies = ref.watch(hobbiesProvider).getAllHobbies();
+            return LabeledWidget(
+              label: 'Hobby',
+              useDarkerLabel: true,
+              child: CustomDropdownField<HobbyModel>.animated(
+                controller: hobbyController,
+                enableSearch: true,
+                hintText: 'Select a hobby',
+                items: {for (var e in hobbies) e.hobby: e},
+                onSelected: (hobby) {},
+              ),
+            );
+          },
         ),
 
         Insets.gapH20,
 
         // Interests Dropdown Filter
-        LabeledWidget(
-          label: 'Interest',
-          useDarkerLabel: true,
-          child: CustomDropdownField<InterestModel>.animated(
-            controller: interestController,
-            enableSearch: true,
-            hintText: 'Select an interest',
-            items: ref
-                .watch(
-                  interestsProvider.select((value) => value.getInterestsMap()),
-                )
-                .map((k, v) => MapEntry(v.interest, v)),
-            onSelected: (id) {},
-          ),
+        Consumer(
+          builder: (context, ref, child) {
+            final interests = ref.watch(interestsProvider).getAllInterests();
+            return LabeledWidget(
+              label: 'Interest',
+              useDarkerLabel: true,
+              child: CustomDropdownField<InterestModel>.animated(
+                controller: interestController,
+                enableSearch: true,
+                hintText: 'Select an interest',
+                items: {for (var e in interests) e.interest: e},
+                onSelected: (interest) {},
+              ),
+            );
+          },
         ),
 
         Insets.gapH20,
 
         // Student Statuses Dropdown Filter
-        LabeledWidget(
-          label: 'Student Status',
-          useDarkerLabel: true,
-          child: CustomDropdownField<int>.animated(
-            controller: statusController,
-            enableSearch: true,
-            hintText: 'Select a status',
-            items: const {
-              'Looking for friends': 1,
-              'Looking for transport': 2,
-              'Looking for lunch pal': 3,
-              'Looking for relationships': 4,
-              'Looking for jamming': 5,
-              'Looking for basketball': 6,
-              'Looking for futsal': 7,
-              'Looking for cricket': 8,
-              'Looking for cards': 9,
-            },
-            onSelected: (id) {},
-          ),
+        Consumer(
+          builder: (context, ref, child) {
+            final studentStatuses =
+                ref.watch(studentStatusesProvider).getAllStudentStatuses();
+            return LabeledWidget(
+              label: 'Student Status',
+              useDarkerLabel: true,
+              child: CustomDropdownField<StudentStatusModel>.animated(
+                controller: statusController,
+                enableSearch: true,
+                hintText: 'Select a status',
+                items: {for (var e in studentStatuses) e.studentStatus: e},
+                onSelected: (studentStatus) {},
+              ),
+            );
+          },
         ),
 
         Insets.gapH20,
