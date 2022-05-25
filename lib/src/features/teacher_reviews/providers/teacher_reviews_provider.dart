a52a5b6187ba784c10e6c2ce9d7fe0993aaea04c
arafaysaleem <a.rafaysaleem@gmail.com>
2022-05-25 07:59:15 +0500

    refact(TeacherReviews): add reviews for miss maria

--- lib/src/features/teacher_reviews/providers/teacher_reviews_provider.dart
+++ lib/src/features/teacher_reviews/providers/teacher_reviews_provider.dart
@@ -201,6 +201,102 @@ class TeacherReviewsProvider {
         graduationYear: 2022,
       ),
     ),
+    TeacherReviewModel(
+      reviewId: 10,
+      learning: 4,
+      grading: 5,
+      attendance: 5,
+      difficulty: 5,
+      overallRating: 4.7,
+      comment: 'Very calm and understanding teacher :)',
+      reviewedAt: DateTime.parse('2022-05-25 01:20:40'),
+      subject: const SubjectModel(
+        subjectCode: 'CS101',
+        subject: 'Database Systems',
+      ),
+      teacherId: 3,
+      reviewedBy: const SubStudentModel(
+        erp: '17855',
+        firstName: 'Rafay',
+        lastName: 'Saleem',
+        profilePictureUrl:
+            'https://avatars.githubusercontent.com/u/62943972?v=4',
+        programId: 1,
+        graduationYear: 2022,
+      ),
+    ),
+    TeacherReviewModel(
+      reviewId: 11,
+      learning: 2,
+      grading: 5,
+      attendance: 5,
+      difficulty: 5,
+      overallRating: 3.5,
+      comment: 'No learning but chillings',
+      reviewedAt: DateTime.parse('2021-08-14 18:35:43'),
+      subject: const SubjectModel(
+        subjectCode: 'CS100',
+        subject: 'Object Oriented Programming',
+      ),
+      teacherId: 3,
+      reviewedBy: const SubStudentModel(
+        erp: '15030',
+        firstName: 'Rafay',
+        lastName: 'Siddiqui',
+        profilePictureUrl:
+            'https://media-exp1.licdn.com/dms/image/C4E03AQHGbNXC5h3ftQ/profile-displayphoto-shrink_200_200/0/1623367978043?e=1657756800&v=beta&t=i96M89rS4ZdeJRq5ee4K3M-24J8Q2om1mi96gDFVJko',
+        programId: 3,
+        graduationYear: 2022,
+      ),
+    ),
+    TeacherReviewModel(
+      reviewId: 12,
+      learning: 5,
+      grading: 5,
+      attendance: 5,
+      difficulty: 5,
+      overallRating: 5,
+      comment: 'Overall amazing teacher. Easy A grade. Worth it!',
+      reviewedAt: DateTime.parse('2022-05-01 09:20:40'),
+      subject: const SubjectModel(
+        subjectCode: 'CS105',
+        subject: 'Data Structures',
+      ),
+      teacherId: 3,
+      reviewedBy: const SubStudentModel(
+        erp: '17855',
+        firstName: 'Rafay',
+        lastName: 'Saleem',
+        profilePictureUrl:
+            'https://avatars.githubusercontent.com/u/62943972?v=4',
+        programId: 1,
+        graduationYear: 2022,
+      ),
+    ),
+    TeacherReviewModel(
+      reviewId: 13,
+      learning: 3,
+      grading: 5,
+      attendance: 4,
+      difficulty: 5,
+      overallRating: 4.6,
+      comment: 'Brilliant Course.',
+      reviewedAt: DateTime.parse('2020-02-19 15:28:59'),
+      subject: const SubjectModel(
+        subjectCode: 'CS103',
+        subject: 'Algorithms',
+      ),
+      teacherId: 3,
+      reviewedBy: const SubStudentModel(
+        erp: '15030',
+        firstName: 'Rafay',
+        lastName: 'Siddiqui',
+        profilePictureUrl:
+            'https://media-exp1.licdn.com/dms/image/C4E03AQHGbNXC5h3ftQ/profile-displayphoto-shrink_200_200/0/1623367978043?e=1657756800&v=beta&t=i96M89rS4ZdeJRq5ee4K3M-24J8Q2om1mi96gDFVJko',
+        programId: 3,
+        graduationYear: 2022,
+      ),
+    ),
   ];
 
   List<TeacherReviewModel> getAllTeacherReviews(int? teacherId) {
